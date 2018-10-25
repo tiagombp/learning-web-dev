d2c2f3dc6278c15b053cd2b0452a6cb4d08c9360
tiagombp <tiagombp@gmail.com>
2018-10-25 15:58:38 -0300

    commit antes de novo branch

--- project-calculator/calculator.js
+++ project-calculator/calculator.js
@@ -11,14 +11,24 @@ corpoCalculadora.addEventListener('click', function(event) {
     console.log("clicou em", event.target.tagName, event.target.className);
     /* event.target.className é uma string! */
 
+        // clicou em botão numérico
+
         if (event.target.className.includes("botaoAlgarismo")) {
             if (saida === "0") {
                 saida = event.target.innerText;
             } else {
                 saida += event.target.innerText;
             }
+    
+        // clicou no CLEAR
+
         } else if (event.target.className.includes("botaoClear")) {
-            saida = "0";
+            saida = "0";     // reinicia tudo
+            resultado = 0;
+            operador = "";
+
+        // clicou no BACKSPACE
+        
         } else if (event.target.className.includes("botaoBackspace")) {
             console.log("Estou aqui. O tamanho da string é... ", saida.length)
             if (saida.length === 1) {
@@ -26,7 +36,10 @@ corpoCalculadora.addEventListener('click', function(event) {
             } else {
                 saida = saida.substring(0,saida.length-1);
             }
-        } else if ((event.target.className.includes("operador")) {
+
+        // clicou em operador
+
+        } else if (event.target.className.includes("operador")) {
 
             if (operador === "") { /* ainda não clicou em nenhum operador */
                 resultado = parseInt(saida);
@@ -49,16 +62,17 @@ corpoCalculadora.addEventListener('click', function(event) {
                     case '-':
                       resultado = resultado - operando;
                       break;   
-                }
-
+                };
                 saida = "0";
-
             }
 
-            if (event.target.className.includes("botaoDivisao")) {
-                
-            }
+        // clicou no igual
+
+        } else if (event.target.className.includes("botaoIgual")) {
+            saida = resultado;
         }
+
+
     visor.innerText = saida;
 });
 
