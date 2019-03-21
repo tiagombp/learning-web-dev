bde8e0849399fcf2b37635409a3a936a10510a16
tiagombp <tiagombp@gmail.com>
2019-03-21 17:35:06 -0300

    updating

--- js-bianca/course-notes.md
+++ js-bianca/course-notes.md
@@ -47,3 +47,81 @@ console.log(x);
 
 ## List Transforms
 
+Nesting
+```javascript
+
+// object literal
+
+const game = {
+    'suspects': [
+        {
+            name: "Rusty",
+            color: "orange"
+        },
+        {
+            name: "Miss Scarlet",
+            color: "red"
+        }
+    ]
+};
+
+// with push
+
+const game = {};
+
+game['suspects'] = [];
+
+game.suspects.push(
+    {
+        name: "Rusty",
+        color: "orange"
+    });
+
+game.suspects[1] = {
+    name: "Miss Scarlet",
+    color: "red"
+};
+
+//
+
+game["suspects"]; // [{name: "Rusty",`color: "orange"}, {name: "Miss Scarlet", color: "red"}]
+```
+
+Objects do not have `length`.
+
+## forEach()
+
+```javascript
+function CreateSuspectObjects(name) {
+  return {
+    name: name,
+    color: name.split(' ')[1],
+    speak: function () { 
+      console.log("my name is", name); 
+    }
+  };
+};
+
+//ES6 version --> speak, the method, is declared directly, no need of ":" and "function" 
+function CreateSuspectObjectsES6(name) {
+  return {
+    name: name,
+    color: name.split(' ')[1],
+    speak() { 
+      console.log("my name is", name); 
+    }
+  };
+};
+
+var suspects_names = ['Miss Scarlet', 'Colonel Mustard', 'Mr. White'];
+
+// obs:
+"oi pessoal".split(' ')
+// => ["oi", "pessoal"]
+
+// se fazemos...
+a = CreateSuspectObjects("Miss Scarlet")
+a.speak(); // => "my name is Miss Scarlet.
+
+let suspects = [];
+```
--- js-bianca/exercise01_game_character.js
+++ js-bianca/exercise01_game_character.js
@@ -26,3 +26,87 @@ let {room, weapon} = combination;
 
 room; // "Kitchen"
 weapon; // "candlestick"
+
+/////////////////////////////////////////////////////////////////////////////////////////
+// Loop exercises
+/////////////////////////////////////////////////////////////////////////////////////////
+
+// 3. given
+
+const game = {
+    'suspects': [
+        {
+            name: "Rusty",
+            color: "orange"
+        },
+        {
+            name: "Miss Scarlet",
+            color: "red"
+        }
+    ]
+};
+
+// loop through "suspects"
+
+let sus = game['suspects'];
+
+for (let i = 0; i < sus.length; i++) {
+    console.log(sus[i]);
+}
+
+// this would algo work
+for (let key in sus) {
+    console.log(sus[key])};
+
+
+// 4. Now loop through all the properties of the suspect objects in the suspects array, mark them if you think they are guilty.
+
+for (let key in sus) {
+    for (let prop in sus[key]) {
+        console.log("Element ", key, "| Property:", prop, "| Value:", sus[key][prop]); //.prop aqui retornaria "undefined"... pq?
+    }
+}
+
+// marcando os culpados
+
+for (let key in sus) {
+    for (let prop in sus[key]) {
+        if (prop == "color") {
+            if (sus[key][prop] == "red") {
+                sus[key]["guilty"] = "yes";
+                console.log("Ha! Guilty!");
+            } else {
+                sus[key]["guilty"] = "no";
+            }
+        }
+        console.log("Element ", key, "| Property:", prop, "| Value:", sus[key][prop]); //.prop aqui retornaria "undefined"... pq?
+    }
+}
+
+// 5. Now destructure this nested data structure into two variables with the strings 'red' and 'orange'.
+
+let [{color: color1}, {color: color2}] = sus; // game.suspects; 
+
+///////////////////////////////////////////////////////////////////////////////////////
+// List transformations
+
+function CreateSuspectObjects(name) {
+    return {
+      name: name,
+      color: name.split(' ')[1],
+      speak() { 
+        console.log("my name is", name); 
+      }
+    };
+  };
+  
+let suspects_names = ['Miss Scarlet', 'Colonel Mustard', 'Mr. White'];
+  
+let suspectsList = [];
+
+// let's "hydrate" this list
+
+for (let i = 0; i < suspects_names.length; i++) {
+    suspect = CreateSuspectObjects(suspects_names[i]);
+    suspectsList.push(suspect);
+}
